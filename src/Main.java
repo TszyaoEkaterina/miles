78c9ca14976e3f8257cc8686950a1d3355463793
Ekaterina <3022805283@qq.com>
2022-02-05 12:38:02 +0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100;
+        int rublePerMile = 20;
+        int miles = ticketPrice / rublePerMile;
+        System.out.println("количество начисленных милль:" + miles);
+    }
+}
